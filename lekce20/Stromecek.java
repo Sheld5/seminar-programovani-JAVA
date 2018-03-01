5862b5c12649c5e888f59798569e2378bad44a5b
Sheld5 <simonsoldat@seznam.cz>
2018-03-01 20:12:17 +0100

    Stromecek.java

--- lekce20/Stromecek.java
+++ lekce20/Stromecek.java
@@ -1,23 +1,27 @@
 import java.io.File;
  
 public class Stromecek {
-    public static void main(String[] args) {
-        printFiles(new File(".."), "");
-    }
- 
-    private static void printFiles(File dir, String indent) {
-        File[] files = dir.listFiles();
-        int count = 0;
-        for (File f : files) {
-            count ++;
-            if (count == files.length) {
-                System.out.printf("%s'-- %s\n", indent, f.getName());
-            } else {
-                System.out.printf("%s|-- %s\n", indent, f.getName());
-            }
-            if (f.isDirectory()) {
-                printFiles(f, indent + "    ");
-            }
-        }
-    }
+	public static void main(String[] args) {
+		printFiles(new File(".."), "");
+	}
+
+	private static void printFiles(File dir, String indent) {
+		File[] files = dir.listFiles();
+		int count = 0;
+		for (File f : files) {
+			count ++;
+			if (count == files.length) {
+				System.out.printf("%s'-- %s\n", indent, f.getName());
+			} else {
+				System.out.printf("%s|-- %s\n", indent, f.getName());
+			}
+			if (f.isDirectory()) {
+				if (count == files.length) {
+					printFiles(f, indent + "     ");
+				} else {
+					printFiles(f, indent + "|    ");
+				}
+			}
+		}
+	}
 }
